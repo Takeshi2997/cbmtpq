3dc35b006d190752a88ed22409af0f5a2c068c70
condensed matter <condensed_matter@condensed-no-iMac.local>
2020-03-04 06:51:21 +0900

    3 layers

--- ann.jl
+++ ann.jl
@@ -8,16 +8,14 @@ resσ(x::Float64)  = x + σ(x)
 
 reallayer1 = Dense(Const.layer[1], Const.layer[2], relu) |> f64
 reallayer2 = Dense(Const.layer[2], Const.layer[3], relu) |> f64
-reallayer3 = Dense(Const.layer[3], Const.layer[4], relu) |> f64
-reallayer4 = Dense(Const.layer[4], Const.layer[5]) |> f64
-f = Chain(reallayer1, reallayer2, reallayer3, reallayer4)
+reallayer3 = Dense(Const.layer[3], Const.layer[4]) |> f64
+f = Chain(reallayer1, reallayer2, reallayer3)
 realps = params(f)
 
 imaglayer1 = Dense(Const.layer[1], Const.layer[2], relu) |> f64
 imaglayer2 = Dense(Const.layer[2], Const.layer[3], relu) |> f64
-imaglayer3 = Dense(Const.layer[3], Const.layer[4], relu) |> f64
-imaglayer4 = Dense(Const.layer[4], Const.layer[5]) |> f64
-g = Chain(imaglayer1, imaglayer2, imaglayer3, imaglayer4)
+imaglayer3 = Dense(Const.layer[3], Const.layer[4]) |> f64
+g = Chain(imaglayer1, imaglayer2, imaglayer3)
 imagps = params(g)
 
 function save(filename1, filename2)
--- setup.jl
+++ setup.jl
@@ -16,7 +16,7 @@ const iϵmax = 20
 const num = 20000
 
 # Network Params
-const layer = [dimB, 128, 128, 128, dimS]
+const layer = [dimB, 128, 128, dimS]
 const layers_num = size(layer)[1] - 1
 
 # Learning Rate
