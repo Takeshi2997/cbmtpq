65006b3b4d2b6a10c6c415a1d20eda5e5bee26cc
condensed matter <condensed_matter@condensed-no-iMac.local>
2020-03-18 13:26:48 +0900

    heisenberg ferro

--- setup.jl
+++ setup.jl
@@ -10,8 +10,8 @@ const J = 1.0f0
 
 # Repeat Number
 const burnintime = 100
-const iters_num = 500
-const it_num = 1000
+const iters_num = 200
+const it_num = 3000
 const iϵmax = 20
 const num = 2000
 
@@ -20,6 +20,6 @@ const layer = [dimB, 96, 96, 96, dimS]
 const layers_num = size(layer)[1] - 1
 
 # Learning Rate
-const lr = 0.00005f0
+const lr = 0.0001f0
 
 end
